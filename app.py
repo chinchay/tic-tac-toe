08dcbb4a575903f9c028acbf68e398373a6e61d3
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2023-06-17 16:45:05 -0300

    chore: Clean code

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
-from flask import Flask, render_template, request, jsonify
-from connection import getPlayer, getSquarePartnerTapped
-# from connection import promptPlayer
+from flask import Flask, render_template, request
 from connection import Channel
-import threading
-
-import socket
-import threading
-from threading import Thread
-import os
-import sys
 
 app = Flask(__name__)
 
-# channelThread = threading.Thread(target=)
-# channel = Channel()
-# channel.start()
-
 @app.route("/")
 def index():
-    # player =  promptPlayer() #getPlayer() # "A" or "B"
-    player =  channel.player #getPlayer() # "A" or "B"
-    # squarePartnerTapped = channel  #getSquarePartnerTapped()
+    player =  channel.player # "1" or "2"
 
-    return render_template(
-        "index.html",
-        player = player,
-        # squarePartnerTapped = squarePartnerTapped
-    )
+    return render_template( "index.html", player = player)
 #
 
 @app.route("/send", methods=["POST"])
@@ -43,41 +24,11 @@ def send():
 @app.route("/receive")
 def receive():
     message = channel.ReceiveMessage()
-    # message = {"message": message}
-    # return jsonify(message=message)
     return message
-
-# def start_server():
-#     host = "192.168.0.218"
-#     port = 8000
-
-#     server_socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-#     server_socket.bind( (host, port) )
-#     server_socket.listen(1)
-#     print("server listening on host: ", host, " and port: ", port)
-
-        
-#     # connection, address = server_socket.accept()
-#     # return connection
-
-#     return server_socket
+#
 
 if __name__ == "__main__":
-
-    # host = "192.168.0.218"
-    # port = 8000
-
-    # server_socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    # server_socket.bind( (host, port) )
-    # server_socket.listen(1)
-    # print("server listening on host: ", host, " and port: ", port)
-
-    # channel = None
-    # if (channel is not None):
-    #     channel = Channel()
-
     channel = Channel()
-    # print("hola")
 
     if (channel.player == "1"):
         app.run( port=5001)
--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,5 @@
-
-// player = "B"
-// `player` is defined in app.py, and read by index.html in <script> tags
 function handleOnclick(){
-
-    // var id = document.getSelection().anchorNode.parentElement.id
-    // var element = document.getElementById(id)
-
     // `player` is defined in index.html as `player = "{{player}}"`
-    
 
     var element = document.getSelection().anchorNode
     element.style.backgroundColor = ( player === "1" ) ? "blue" : "red"
@@ -17,7 +9,6 @@ function handleOnclick(){
 
     // now you are ready to listen
     listen()
-
 
     // send this `id` to the other player so he can color it in his screen  
     fetch(
@@ -31,24 +22,10 @@ function handleOnclick(){
 
             body: id
         }
-    
     )
-
-
 }
 
 function listen(){
-    // const id = document.getElementById("whatId").value
-    // console.log("the other player has clicked on: ", id)
-    // var element = document.getElementById(id)
-    // element.style.backgroundColor = ( player === "A" ) ? "red" : "blue"
-
-    // // `squarePartnerTapped` is defined in index.html
-    // // as `squarePartnerTapped = "{{squarePartnerTapped}}"`
-    // console.log("the other player has clicked on: ", squarePartnerTapped)
-    // var element = document.getElementById(squarePartnerTapped)
-    // element.style.backgroundColor = ( player === "A" ) ? "red" : "blue"
-
     fetch('/receive')
     .then(function (response) {
         return response.text();
@@ -58,22 +35,7 @@ function listen(){
 
         var element = document.getElementById(text)
         element.style.backgroundColor = ( player === "1" ) ? "red" : "blue"
-    
     })
-
-
-
 }
 
-console.log("hi there")
-
 listen()
-
-// if (player == "2") {
-//     listen()
-// }
-
-// var element = document.getElementById("submitWhatId")
-// element.addEventListener( "click", () => autoClick() )
-
-// https://healeycodes-com.translate.goog/talking-between-languages?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt-BR&_x_tr_pto=sc&_x_tr_hist=true
